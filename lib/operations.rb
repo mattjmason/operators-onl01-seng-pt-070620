3ba52f7bc1c75c45367982347f2f62a9061f5bf1
Matt Mason <masonmj10@gmail.com>
2020-07-07 21:57:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,8 +7,8 @@ end
 
 speed = 50
 def not_safe?(speed)
-if	speed > 60 ? "true" : "false"
-else speed < 40 ? "true" : "false"
+	speed > 60 ? "true" : "false"
+  speed < 40 ? "true" : "false"
 	end
 end
 	
